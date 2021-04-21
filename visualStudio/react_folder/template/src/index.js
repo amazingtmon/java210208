8f94bf24ac06840ce77d5b9ef552472d44bff2fa
amazingtmon <webtree12@gmail.com>
2021-04-21 12:03:07 +0900

    react file commit

--- visualStudio/react_folder/template/src/index.js
+++ visualStudio/react_folder/template/src/index.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import App from './app';
 
+//'use strict';
 ReactDOM.render(
   <React.StrictMode>
     <App />

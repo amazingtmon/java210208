1c588932aa54b5c19676290dddc63507a20e1e5a
Sang Chul <webtree12@gmail.com>
2021-05-16 15:56:39 +0900

    vscode edit portfolio - add bgTitleAction

--- visualStudio/portfolio_kosmo/portfolio_ysc/src/scrollAction.js
+++ visualStudio/portfolio_kosmo/portfolio_ysc/src/scrollAction.js
@@ -1,17 +1,17 @@
 const navbar = document.querySelector('.navbar');
 const start_pageYOffset = window.pageYOffset;
+const about_bgtitle = document.querySelector('.about__background__title');
 
 export default class Navbar {
   constructor(){
     this.navbar = document.querySelector('.navbar');
   }
   
-  scrollAction() {
+  navbarAction() {
     let current_value = window.pageYOffset;
     let moved_value = start_pageYOffset;
-    console.log('start '+start_pageYOffset);
-    console.log('current '+current_value);
-    console.log(window.innerHeight);
+    //console.log('start '+start_pageYOffset);
+    //console.log('current '+current_value);
     if (window.pageYOffset > navbar.clientHeight) {
       navbar.classList.add('nav_opacity');
       navbar.classList.add('nav_hide');
@@ -20,6 +20,7 @@ export default class Navbar {
     }
   }
 
+  
   mouseOverAction() {
     console.log('mouse on');
     navbar.classList.remove('nav_opacity');
@@ -34,8 +35,31 @@ export default class Navbar {
     }
   }
   
+  bgTitleAction(){
+    const about__section = document.querySelector('.about__section');
+    const career__section = document.querySelector('.career__section');
+    const about_bgtitle = document.querySelector('.about__background__title');
+    let sectionTop = about__section.getBoundingClientRect().top;
+    let sectionTop2 = career__section.getBoundingClientRect().top;
+    console.dir(about__section);
+    console.log(window.pageYOffset);
+    console.log('section top: '+sectionTop);
+    console.log('section bottom: '+about__section.getBoundingClientRect().bottom);
+    console.log('about_sec cord: '+Math.round(window.pageYOffset + sectionTop));
+    console.log('career_sec cord: '+Math.round(window.pageYOffset + sectionTop2));
+    console.log('chng cord: '+Math.round(window.pageYOffset - sectionTop));
+    if(Math.round(window.pageYOffset - sectionTop) > 0){
+      console.log('add classList');
+      about_bgtitle.classList.add('bgtitle_transition_LtoR');
+    } else {
+      about_bgtitle.classList.remove('bgtitle_transition_LtoR');
+    }
+
+  }
+  
   init() {
-    window.addEventListener('scroll', this.scrollAction);
+    window.addEventListener('scroll', this.navbarAction);
+    window.addEventListener('scroll', this.bgTitleAction);
     navbar.addEventListener('mouseover', this.mouseOverAction);
     navbar.addEventListener('mouseleave', this.mouseLeaveAction);
   }
